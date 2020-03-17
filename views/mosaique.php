76374e7eb828d6758d29f18360bb01b93d92110e
Mr-Aylex <carmonealexandre@gmail.com>
2020-03-17 11:45:54 +0100

    bug de git corriger (ligne de code qui disparait)

--- views/mosaique.php
+++ views/mosaique.php
@@ -31,92 +31,24 @@ $tab_film = $manager->flush_film();
 
   <section class="section section-a">
     <div class="container">
-      <table style="">
-      <tr>
-      <td>
+        <table style="">
+            <tr>
 
-      <div class="division bas" style="width: 18rem;">
-
-        <a href="../views/film/film_la_foret.php"><img class="img" height="200" width="150"src="../src/images/image_film/Appel_de_la_foret.jpg" class="" alt="..."/></a>
-
-      <span class="icon" >Films</span>
-        <div class="card-body">
-          <h5 class="titre_film card-title">L'appel de la forêt</h5>
-
-
-
-        </div>
-      </div>
-      </td>
-
-      <td>
-
-      <div class="division bas" style="width: 18rem;">
-
-        <a href="../views/film/film_the_boy.php"><img class="img" height="200" width="150"src="../src/images/image_film/the_boy.jpeg" class="" alt="..."/></a>
-
-      <span class="icon" >Films</span>
-        <div class="card-body">
-          <h5 class="titre_film card-title">The boy</h5>
-
-
-
-        </div>
-      </div>
-      </td>
-
-
-      <td>
-
-      <div class="division bas" style="width: 18rem;">
-
-        <a href="../views/film/film_sonic.php"><img class="img" height="200" width="150"src="../src/images/image_film/sonic.jpeg" class="" alt="..."/></a>
-
-      <span class="icon" >Films</span>
-        <div class="card-body">
-          <h5 class="titre_film card-title">Sonic le film</h5>
-
-
-
-        </div>
-      </div>
-      </td>
-      <td>
-
-      <div class="division bas" style="width: 18rem;">
-
-        <a href="../views/film/film_bad_boys.php"><img class="img" height="200" width="150"src="../src/images/image_film/bad_boys.jpeg" class="" alt="..."/></a>
-
-      <span class="icon" >Films</span>
-        <div class="card-body">
-          <h5 class="titre_film card-title">Bad Boys 3</h5>
-
-
-
-        </div>
-      </div>
-      </td>
-
-
-      <td>
-
-      <div class="division bas" style="width: 18rem;">
-
-        <a href="../views/film/film_harley_quinn.php"><img class="img" height="200" width="150"src="../src/images/image_film/Harley_quinn.jpeg" class="" alt="..."/></a>
-
-      <span class="icon" >Films</span>
-        <div class="card-body">
-          <h5 class="titre_film card-title">Harley Quinn</h5>
-
-
-
-        </div>
-      </div>
-      </td>
-      </tr>
-
-
-      </table>
+                <?php foreach ($tab_film as $item => $value) {?>
+                    <td>
+                        <div class="division bas" style="width: 18rem;">
+                                <a href="../views/film/film.php?name=<?php echo $item ?>"><img class="img" height="200" width="150"
+                                                                                               src="../src/images/image_film/<?php echo $value->getImage(); ?>"
+                                                                                               class="" alt="..."/></a>
+                                <span class="icon">Films</span>
+                                <div class="card-body">
+                                    <h5 class="titre_film card-title"><?php echo $value->getTitre(); ?></h5>
+                                </div>
+                        </div>
+                    </td>
+                <?php } ?>
+            </tr>
+        </table>
     </div>
   </section>
 
