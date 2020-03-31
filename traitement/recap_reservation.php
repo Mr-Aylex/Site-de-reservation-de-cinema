d9969f1ef5975def87d0a06ea607f3a2d10d51c2
@sulli8 <sullivan.sextius@gmail.com>
2020-03-31 09:19:31 +0200

    test de l'affichage des réservations

---
+++ traitement/recap_reservation.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once($_SERVER['DOCUMENT_ROOT']."/site-de-reservation-de-cinema/manager/manager.php");
+$manager = new manager();
+$id = 11;
+echo $id;
+session_start();
+
+ ?>
--- views/recapitulatif_reservation.php
+++ views/recapitulatif_reservation.php
@@ -1,10 +1,38 @@
-<!DOCTYPE html>
-<html lang="" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
+<html>
+<?php
+include "../include/header.php";
 
-  </body>
+?>
+<body style="background-image: url('../src/images/fonds_de_pages/fond_modification.jpg'); background-size: cover">
+<section class="container">
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th class="text-white">Numéro</th>
+            <th class="text-white">Tarif</th>
+            <th class="text-white">Type_de_tarif</th>
+            <th class="text-white">Id utilisateur</th>
+            <th class="text-white">Salle</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        <tr>
+            <th class="text-white"><?php echo $unique["numero"] ?></th>
+            <th class="text-white"><?php echo $unique["tarif"] ?></th>
+            <th class="text-white"><?php echo $unique["type_de_tarif"]?></th>
+            <th class="text-white"><?php echo $unique["id_utilisateur"] ?></th>
+            <th class="text-white"><?php echo $unique["salle"] ?></th>
+
+        </tr>
+
+
+        </tbody>
+    </table>
+</section>
+
+
+
+</body>
+
 </html>
